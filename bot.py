c6786fa1a2747a62ef701ebdd2ae714b055bff6e
Hiro Ogawa <hiro@ogawa.40sec.net>
2018-06-23 10:04:28 +0900

    initial

---
+++ bot.py
@@ -0,0 +1,66 @@
+import os
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# 環境変数読み込み
+line_messaging_api_token = os.environ['LINE_MESSAGING_API_TOKEN']
+line_messaging_api_secret = os.environ['LINE_MESSAGING_API_SECRET']
+# base_url = os.environ['BOT_BASE_URL']
+
+line_bot_api = LineBotApi(line_messaging_api_token)
+handler = WebhookHandler(line_messaging_api_secret)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+def is_connection_check(event):
+    check_uid = 'Udeadbeefdeadbeefdeadbeefdeadbeef'
+    check_tokens = [
+        '00000000000000000000000000000000',
+        'ffffffffffffffffffffffffffffffff',
+    ]
+    if event.source.type == 'user':
+        if event.source.user_id == check_uid:
+            return True
+    if event.reply_token in check_tokens:
+        return True
+    return False
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if is_connection_check(event):
+        return
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
